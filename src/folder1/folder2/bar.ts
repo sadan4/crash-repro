980df44ccdb9ccfff9f20d78e350032129bfc269
sadan <117494111+sadan4@users.noreply.github.com>
2025-04-18 10:51:36 -0400

    cleanup

--- src/folder1/folder2/bar.ts
+++ src/folder1/folder2/bar.ts
@@ -2,11 +2,7 @@ import { Crasher } from "..";
 
 export class ReferenceProvider {
     async provideReferences(document, position) {
-        try {
             return await new Crasher()
                 .generateReferences(position);
-        } catch (e) {
-            console.error(String(e));
-        }
     }
 }
