597121b6aad18472ff960cbaf651e9b951d3a39b
sadan <117494111+sadan4@users.noreply.github.com>
2025-04-18 11:20:22 -0400

    about as simple as it can get

---
+++ extension.js
@@ -0,0 +1,20 @@
+const vscode = require("vscode");
+
+class Crasher {
+    func(position) {
+        for (const foo of []) {
+            someVar.map(() => [this.anything, foo]);
+        }
+    }
+}
+
+module.exports = {
+    activate(context) {
+        context.subscriptions.push(
+            vscode.commands.registerCommand("crash-repro.hello-crash", () => {
+                new Crasher().func(null);
+            })
+        );
+
+    }
+};
