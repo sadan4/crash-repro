3cbb86359dfaca403f2d4d551a141189d41ac9cd
sadan <117494111+sadan4@users.noreply.github.com>
2025-04-18 10:53:46 -0400

    variable not needed

--- src/folder1/foo.ts
+++ src/folder1/foo.ts
@@ -11,7 +11,7 @@ export class Crasher {
         // TODO: support jumping from object literals
         for (const [exportedName] of [] as string[][]) {
 
-            const left = where.sync.map((x) => [x, this.moduleId, exportedName]);
+            const left = where.sync.map(() => [this.moduleId, exportedName]);
             
         }
     }
